46ac3f253d4d8e65f9c81d711acfba27a617a5cf
232-ReemaNasser <66470185+232-ReemaNasser@users.noreply.github.com>
2020-06-27 20:39:23 +0300

    .

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,25 +1,31 @@
 // السؤال الأول
 // 1
-
+let name = "Reema";
 // 2
-
+let number = 16;
 // 3 and 4
-
+console.log(name + " " + number)
 // 5 and 6
+console.log(number*5)
+
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies = ["painting","swimming","cooking"]
 // 2
-
+ hobbies.push(number)
+ console.log(hobbies)
 // 3
-
+let object={
+    road:"web", lang:"css"};
+    console.log(object)
 // 4
 
 // 5
-
+let objectta=["Rahaf"]
+console.log(objectta)
 // 6
 
 // 7
